0152921532e9d77e8401734ff124f299d796dac8
matiasgramkow <matias_gramkow@hotmail.com>
2021-07-22 15:35:03 +0200

    new aws deployment settings

--- server/keys.js
+++ server/keys.js
@@ -6,4 +6,10 @@ module.exports = {
   pgDatabase: process.env.PGDATABASE,
   pgPassword: process.env.PGPASSWORD,
   pgPort: process.env.PGPORT,
+  mysqlHost: process.env.MYSQL_HOST,
+  mysqlPort: process.env.MYSQL_PORT,
+  mysqlUser: process.env.MYSQL_USER,
+  mysqlRootPassword: process.env.MYSQL_ROOT_PASSWORD,
+  mysqlDatabase: process.env.MYSQL_DATABASE,
+  mysqlPassword: process.env.MYSQL_PASSWORD
 };
